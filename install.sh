5916c93f73c9c1caaf29a7c24e1150cef37dbf44
topgg <sumxmut@gmail.com>
2020-09-23 18:09:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo mkdir /scripts 2>/dev/null && sudo chmod 755 /scripts
 sudo cd /scripts
-sudo wget -O --no-check-certificate  https://raw.githubusercontent.com/topgg/smtp-watcher/master/smtpwatcher.sh && sudo chmod 755 smtpwatcher.sh
+sudo wget --no-check-certificate  https://raw.githubusercontent.com/topgg/smtp-watcher/master/smtpwatcher.sh -O smtpwatcher.sh && sudo chmod 755 smtpwatcher.sh
 sudo crontab -l > conf ; echo " */2 * * * * root sh /scripts/smtpwatcher.sh" >> conf && crontab conf && rm -rf conf 
 sudo mkdir /var/log/crontab 2>/dev/null
 sudo touch /var/log/crontab/smtpwatcher.log && sudo chmod 666 /var/log/crontab/smtpwatcher.log

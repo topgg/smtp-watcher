4857dc594825abc055f86ad8324c007d6a24e5bf
topgg <sumxmut@gmail.com>
2020-10-22 16:34:47 +0800

    Update smtpwatcher.sh

--- smtpwatcher.sh
+++ smtpwatcher.sh
@@ -39,7 +39,7 @@ then
     reloadpostfix="`(whereis postfix|awk -F' ' '{print$2}')` reload"
     if [ ${number[0]} -eq 0 ]  #如果没有该进程，则重启
     then
-        sudo $postfixpath 1>&2 >> $file_name
+        sudo $reloadpostfix 1>&2 >> $file_name
         #启动程序的命令
         echo '异常,执行重启Postfix', `date` >> $file_name  #把重启的进程号、时间 写入日志
     fi
@@ -47,14 +47,14 @@ then
     if [ ${number[1]} -eq 0 ]  #如果没有该进程，则重启
     then
 
-        sudo /usr/sbin/saslauthd -m /var/run/saslauthd -a pam 1>&2 >> $file_name
+        /usr/sbin/saslauthd -m /var/run/saslauthd -a pam 1>&2 >> $file_name
         #启动程序的命令
         echo '异常,执行重启saslauth', `date` >> $file_name  #把重启的进程号、时间 写入日志
     fi
 
     if [ ${number[2]} -eq 0 ]  #如果没有该进程，则重启
     then
-        sudo /usr/sbin/opendkim -x /etc/opendkim.conf -P /var/run/opendkim/opendkim.pid 1>&2 >> $file_name   #启动程序的命令
+        /usr/sbin/opendkim -x /etc/opendkim.conf -P /var/run/opendkim/opendkim.pid 1>&2 >> $file_name   #启动程序的命令
         echo '异常,执行重启opendkim', `date` >> $file_name  #把重启的进程号、时间 写入日志
     fi
 fi
